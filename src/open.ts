e19315bbb158039a319877669bd5e6c0d4c872e9
nekn3x <43030839+NEKN3X@users.noreply.github.com>
2025-06-11 12:32:22 +0900

    fix open

--- src/open.ts
+++ src/open.ts
@@ -1,13 +1,5 @@
 import { open } from "https://deno.land/x/open/index.ts";
 
 export async function openUrl(url: string) {
-  // Windows限定かは分からないけど、openが遅いので直接startを呼ぶ
-  if (Deno.build.os === "windows") {
-    const command = new Deno.Command("cmd", {
-      args: ["/c", "start", url],
-    });
-    await command.output();
-  } else {
-    await open(url);
-  }
+  await open(url, { wait: true });
 }
